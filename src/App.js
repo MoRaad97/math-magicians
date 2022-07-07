b10d5bef68d4593b4a9fcc7c2a21607b624a47ed
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-07 20:01:04 +0300

    Refactor the code using hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import Calculator from './Calculator/calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
-}
+const App = () => (
+  <Calculator />
+);
 
 export default App;
