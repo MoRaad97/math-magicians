329428e7ade2337c4438f8ecdfd2d6ea60c8ff45
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-07 19:31:17 +0300

    Adding events and logic to the calculator

--- src/Calculator/calculator.js
+++ src/Calculator/calculator.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import operate from '../logic/operate'
-import calculate from '../logic/calculate'
+import calculate from '../logic/calculate';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.result = this.result.bind(this);
+  }
+
+  result(e) {
+    const { obj } = this.state;
+    const newObj = calculate(obj, e.target.textContent);
+    this.setState({ obj: newObj });
+  }
+
   render() {
+    const { obj } = this.state;
     return (
       <>
         <div className="calculator-container">
-          <div className="screen"><span className="result">0</span></div>
+          <div className="screen"><span className="result">{obj.next || obj.total || 0}</span></div>
           <div className="buttons">
-            <button type="button" className="normal">AC</button>
-            <button type="button" className="normal">+/-</button>
-            <button type="button" className="normal">%</button>
-            <button type="button" className="normal active">÷</button>
-            <button type="button" className="normal">7</button>
-            <button type="button" className="normal">8</button>
-            <button type="button" className="normal">9</button>
-            <button type="button" className="normal active">x</button>
-            <button type="button" className="normal">4</button>
-            <button type="button" className="normal">5</button>
-            <button type="button" className="normal">6</button>
-            <button type="button" className="normal active">-</button>
-            <button type="button" className="normal">1</button>
-            <button type="button" className="normal">2</button>
-            <button type="button" className="normal">3</button>
-            <button type="button" className="normal active">+</button>
-            <button type="button" className="zero">0</button>
-            <button type="button" className="normal">.</button>
-            <button type="button" className="normal active">=</button>
+            <button type="button" className="normal" onClick={this.result}>AC</button>
+            <button type="button" className="normal" onClick={this.result}>+/-</button>
+            <button type="button" className="normal" onClick={this.result}>%</button>
+            <button type="button" className="normal active" onClick={this.result}>÷</button>
+            <button type="button" className="normal" onClick={this.result}>7</button>
+            <button type="button" className="normal" onClick={this.result}>8</button>
+            <button type="button" className="normal" onClick={this.result}>9</button>
+            <button type="button" className="normal active" onClick={this.result}>x</button>
+            <button type="button" className="normal" onClick={this.result}>4</button>
+            <button type="button" className="normal" onClick={this.result}>5</button>
+            <button type="button" className="normal" onClick={this.result}>6</button>
+            <button type="button" className="normal active" onClick={this.result}>-</button>
+            <button type="button" className="normal" onClick={this.result}>1</button>
+            <button type="button" className="normal" onClick={this.result}>2</button>
+            <button type="button" className="normal" onClick={this.result}>3</button>
+            <button type="button" className="normal active" onClick={this.result}>+</button>
+            <button type="button" className="zero" onClick={this.result}>0</button>
+            <button type="button" className="normal" onClick={this.result}>.</button>
+            <button type="button" className="normal active" onClick={this.result}>=</button>
           </div>
         </div>
       </>
