aaf3f804d5a8722739b5f8531c811dd7ca91f60d
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-13 12:56:44 +0300

    Fix The tests

--- src/__test__/calculator.test.js
+++ src/__test__/calculator.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Calculator from '../Components/calculator';
 
-it.only('render the Calculator Component', () => {
+it('render the Calculator Component', () => {
   const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
 });
